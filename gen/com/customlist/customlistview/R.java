35f67737350969366e27f15ae1af9e2d1d6483f9
nishashirawala <nishashirawala@gmail.com>
2013-12-18 22:16:26 -0500

    created mock data for list view

--- gen/com/customlist/customlistview/R.java
+++ gen/com/customlist/customlistview/R.java
@@ -32,21 +32,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000a;
-        public static final int divider1=0x7f090002;
-        public static final int divider2=0x7f090004;
-        public static final int divider3=0x7f090006;
-        public static final int divider4=0x7f090008;
-        public static final int mainString=0x7f090000;
-        public static final int smallText1=0x7f090001;
-        public static final int smallText2=0x7f090003;
-        public static final int smallText3=0x7f090005;
-        public static final int smallText4=0x7f090007;
-        public static final int smallText5=0x7f090009;
+        public static final int action_settings=0x7f09000c;
+        public static final int divider1=0x7f090004;
+        public static final int divider2=0x7f090006;
+        public static final int divider3=0x7f090008;
+        public static final int divider4=0x7f09000a;
+        public static final int list=0x7f090000;
+        public static final int mainString=0x7f090002;
+        public static final int smallText1=0x7f090003;
+        public static final int smallText2=0x7f090005;
+        public static final int smallText3=0x7f090007;
+        public static final int smallText4=0x7f090009;
+        public static final int smallText5=0x7f09000b;
+        public static final int textview=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int route_time_list_view=0x7f030001;
+        public static final int list_layout=0x7f030001;
+        public static final int route_time_list_view=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- src/com/customlist/customlistview/MainActivity.java
+++ src/com/customlist/customlistview/MainActivity.java
@@ -1,8 +1,15 @@
 package com.customlist.customlistview;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ListView;
+
+import com.customlist.adapter.CustomListAdapter;
+import com.customlist.adapter.ListVO;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +17,33 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ListView list = (ListView) findViewById(R.id.list);
+		List<ListVO> listVOs = createMockData();
+		CustomListAdapter adapter = new CustomListAdapter(this, listVOs);
+		list.setAdapter(adapter);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private List<ListVO> createMockData() {
+		List<ListVO> listVOs = new ArrayList<ListVO>();
+
+		for (int index = 1; index <= 10; index++) {
+			ListVO vo = new ListVO();
+			vo.setRouteName("list-item-" + index);
+			List<String> subItemList =  new ArrayList<String>();
+			for (int sub_index = 1; sub_index <= 5; sub_index++) {
+				subItemList.add("sub-item-" + sub_index);
+			}
+			vo.setTimeList(subItemList);
+			listVOs.add(vo);
+		}
+
+		return listVOs;
 	}
 
 }
